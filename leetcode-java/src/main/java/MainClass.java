93adc2b80673c2fb95cfe4056d658aecac21f91e
Ashique Shereef <ashiqueshereef@Ashiques-MacBook-Pro.local>
2024-09-14 20:10:50 +0530

    Restructured project

---
+++ leetcode-java/src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+    public static void main(String[] args) {
+        // uncomment to run the code you need
+//        MergeStringsAlternatively mergeStringsAlternatively = new MergeStringsAlternatively();
+//        String result = mergeStringsAlternatively.mergeAlternately("abc","pqr");
+//        System.out.println(result);
+
+
+    }
+}
